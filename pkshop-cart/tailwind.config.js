11e41f9c95498551904065b8518d473e20ba2654
Hoàng Phúc <113100226+hoangphuc38@users.noreply.github.com>
2024-11-14 22:59:30 +0700

    [CART] Initial cart page

--- pkshop-cart/tailwind.config.js
+++ pkshop-cart/tailwind.config.js
@@ -5,15 +5,17 @@ module.exports = {
   ],
   theme: {
     fontSize: {
-      normal: '20px',
+      normal: '16px',
       title: '25px',
-      info: '40px'
+      info: '40px',
+
     },
     colors: {
       primary: '#125B9A',
       button: '#D9D9D9',
       white: '#FFFFFF',
-      orange: '#FF6525'
+      orange: '#FF6525',
+      placeholder: '#A59A9A'
     }
   },
   plugins: [],
