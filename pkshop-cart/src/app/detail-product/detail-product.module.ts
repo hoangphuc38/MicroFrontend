7714187a990b8db5b2622640f16099d497f34c9b
Hoàng Phúc <113100226+hoangphuc38@users.noreply.github.com>
2024-11-19 22:13:16 +0700

    module federation for Angular

---
+++ pkshop-cart/src/app/detail-product/detail-product.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { DetailProductComponent } from "./detail-product.component";
+
+
+@NgModule({
+    declarations: [
+        DetailProductComponent
+    ],
+    imports: [
+        CommonModule
+    ]
+})
+
+export class DetailProductModule { }
