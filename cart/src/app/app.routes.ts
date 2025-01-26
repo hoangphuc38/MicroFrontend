d2b58f484d4b1e5928f592d2d985a54028364ccd
Hoàng Phúc <113100226+hoangphuc38@users.noreply.github.com>
2025-01-26 23:37:33 +0700

    [Cart] Handle UI and apply global font

--- cart/src/app/app.routes.ts
+++ cart/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { OrderComponent } from './pages/order/order.component';
 
 export const routes: Routes = [
     { path: '', component: CartComponent },
-    { path: 'cart', component: CartComponent },
+    { path: 'order', component: OrderComponent },
     { path: '**', component: EmptyRouteComponent }
 ];
