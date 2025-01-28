1b18b52fb82ad68b2d1981dd937d3eab9c60d87d
DKhiem017 <100778307+DKhiem017@users.noreply.github.com>
2025-01-28 11:09:24 +0700

    New css to Nav-bar and add Logo to Nav-bar

--- nav-bar/src/root.component.tsx
+++ nav-bar/src/root.component.tsx
@@ -1,23 +1,27 @@
 import "./root.component.css";
 import { BrowserRouter, Link } from "react-router-dom";
+import logo from "../src/asset/images/Logo.png";
 
 export default function Root(props) {
-  return <BrowserRouter>
-
-    <nav className="nav">
-      <Link to="/" className="link">
-        Home
-      </Link>
-      <Link to="/product" className="link">
-        Product
-      </Link>
-      <Link to="/sales" className="link">
-        Sales
-      </Link>
-      <Link to="/auth/logout" className="link">
-        Logout
-      </Link>
-    </nav>
-  </BrowserRouter>
-
+  return (
+    <BrowserRouter>
+      <nav className="nav">
+        <Link to="/" className="link home-link">
+          <img src={logo} className="link-icon"></img>
+          <text>PKShop</text>
+        </Link>
+        <div>
+          <Link to="/product" className="link">
+            Product
+          </Link>
+          <Link to="/sales" className="link">
+            Cart
+          </Link>
+        </div>
+        <Link to="/auth/logout" className="link">
+          Logout
+        </Link>
+      </nav>
+    </BrowserRouter>
+  );
 }
