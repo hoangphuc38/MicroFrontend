8c4a3afd147c98470088734f0a9e47e54924f089
DKhiem017 <100778307+DKhiem017@users.noreply.github.com>
2025-02-06 17:06:36 +0700

    UI Banner and Sport Item

--- home/src/root.component.tsx
+++ home/src/root.component.tsx
@@ -1,5 +1,7 @@
 import "./root.component.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BannerImg from "../src/asset/images/Banner.png";
+import SportImg from "../src/asset/images/darwin.jpg";
 
 export default function Root(props) {
   // Uncomment và tạo các component cho các route
@@ -17,6 +19,24 @@ export default function Root(props) {
   //   </BrowserRouter>
   // </>;
   return (
-    <div>Hello from React</div>
-  )
+    <div className="container">
+      {/* banner */}
+      <div className="banner">
+        <img src={BannerImg} className="bannerImg"></img>
+      </div>
+      {/* 2 tab */}
+      <div className="category_container">
+        <h3>Choose your favor</h3>
+        {/* sport */}
+        <div className="tab_container">
+          <div className="tabItem">
+            <img className="tabImg" src={SportImg}></img>
+            <div className="sportTab">
+              <p>Sport</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
