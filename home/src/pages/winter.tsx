8d1cf995aac624fc1efc72aac3464c9cc87e74fe
DKhiem017 <100778307+DKhiem017@users.noreply.github.com>
2025-02-08 00:46:59 +0700

    [Winter] add snow flake effect

--- home/src/pages/winter.tsx
+++ home/src/pages/winter.tsx
@@ -1,26 +1,40 @@
 import { useNavigate } from "react-router-dom";
-import '../pages/winter.css'
+import "../pages/winter.css";
 import DropdownMenu from "../components/dropdownMenu";
 import ProductCard from "../components/productCard";
-import imgTest from '../asset/images/liverpool.jpg'
+import imgTest from "../asset/images/liverpool.jpg";
 
 export default function Winter() {
-    const navigate = useNavigate(); // Hook điều hướng
-  
-    return (
-      <div className="winter_container">
-        <div className="link_container">
-            <nav>Home</nav>
-            <span>{'>'}</span>
-            <nav>Sport Products</nav>
-        </div>
-        <div className="filter_container">
-            <h2>Sport Products</h2>
-            <DropdownMenu/>
-        </div>
-        <div className="product-list">
-            <ProductCard imgSrc={imgTest} name="Liverpool Kit 2024" price={500000}/>
-        </div>
+  const navigate = useNavigate(); // Hook điều hướng
+
+  return (
+    <div className="winter_container">
+      <div className="snowflake">❅</div>
+      <div className="snowflake">❅</div>
+      <div className="snowflake">❆</div>
+      <div className="snowflake">❄</div>
+      <div className="snowflake">❅</div>
+      <div className="snowflake">❆</div>
+      <div className="snowflake">❄</div>
+      <div className="snowflake">❅</div>
+      <div className="snowflake">❆</div>
+      <div className="snowflake">❄</div>
+      <div className="link_container">
+        <nav>Home</nav>
+        <span>{">"}</span>
+        <nav>Winter Products</nav>
       </div>
-    );
+      <div className="filter_container">
+        <h2>Winter Products</h2>
+        <DropdownMenu />
+      </div>
+      <div className="product-list">
+        <ProductCard
+          imgSrc={imgTest}
+          name="Liverpool Kit 2024"
+          price={500000}
+        />
+      </div>
+    </div>
+  );
 }
