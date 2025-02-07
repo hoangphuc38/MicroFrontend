1ef640b751193ab1aedc563ffb7c6fc2eb4a0d7c
DKhiem017 <100778307+DKhiem017@users.noreply.github.com>
2025-02-07 22:35:53 +0700

    Split code from Home and add browser router

---
+++ home/src/pages/home.tsx
@@ -0,0 +1,40 @@
+import { useNavigate } from "react-router-dom";
+import BannerImg from "../../src/asset/images/Banner.png";
+import SportImg from "../../src/asset/images/darwin.jpg";
+import WinterImg from "../../src/asset/images/winter-copy.png";
+
+export default function Home() {
+  const navigate = useNavigate(); // Hook điều hướng
+
+  return (
+    <div className="container">
+      {/* banner */}
+      <div className="banner">
+        <img src={BannerImg} className="bannerImg" alt="Banner" />
+      </div>
+      
+      {/* 2 tab */}
+      <div className="category_container">
+        <h3>Choose your favor</h3>
+       
+        <div className="tab_container">
+          {/* sport */}
+          <div className="tabItem" onClick={() => navigate("/sport")}>
+            <img className="tabImg" src={SportImg} alt="Sport" />
+            <div className="sportTab">
+              <p>Sport</p>
+            </div>
+          </div>
+          
+          {/* winter */}
+          <div className="tabItem winterItem" onClick={() => navigate("/winter")}>
+            <img className="tabImg" src={WinterImg} alt="Winter" />
+            <div className="winterTab">
+              <p>Winter</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- home/src/root.component.tsx
+++ home/src/root.component.tsx
@@ -1,50 +1,16 @@
 import "./root.component.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import BannerImg from "../src/asset/images/Banner.png";
-import SportImg from "../src/asset/images/darwin.jpg";
-import WinterImg from "../src/asset/images/winter-copy.png";
+import Home from "./pages/home";
 
 export default function Root(props) {
   // Uncomment và tạo các component cho các route
-  //   return <>
-  //   <BrowserRouter>
-  //     <Routes>
-  //       <Route path="/product/" element={<Layout />}>
-  //         <Route element={<AuthGuardedRoute />}>
-  //           <Route path="/product/list" element={<ProductList />} />
-  //           <Route path="/product/details/:id" element={<ProductDetails />} />
-  //           <Route index element={<ProductList />} />
-  //         </Route>
-  //       </Route>
-  //     </Routes>
-  //   </BrowserRouter>
-  // </>;
-  return (
-    <div className="container">
-      {/* banner */}
-      <div className="banner">
-        <img src={BannerImg} className="bannerImg"></img>
-      </div>
-      {/* 2 tab */}
-      <div className="category_container">
-        <h3>Choose your favor</h3>
-       
-        <div className="tab_container">
-           {/* sport */}
-          <div className="tabItem">
-            <img className="tabImg" src={SportImg}></img>
-            <div className="sportTab">
-              <p>Sport</p>
-            </div>
-          </div>
-          <div className="tabItem winterItem">
-            <img className="tabImg" src={WinterImg}></img>
-            <div className="winterTab">
-              <p>Winter</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    return (
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/sport" element={<Sport />} />
+        <Route path="/winter" element={<Winter />} /> */}
+      </Routes>
+    </BrowserRouter>
+    )
 }
