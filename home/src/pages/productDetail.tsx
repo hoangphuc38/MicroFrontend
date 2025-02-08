e8dcf7f388665eb6ef93666f17b7af42aa271918
DKhiem017 <100778307+DKhiem017@users.noreply.github.com>
2025-02-08 22:22:22 +0700

    Create new component: Size Selector

--- home/src/pages/productDetail.tsx
+++ home/src/pages/productDetail.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import "../pages/productDetail.css";
 import itemImg from '../asset/images/liverpool.jpg'
+import RatingStars from "../components/ratingStar";
 
 export default function ProductDetail() {
   const { id } = useParams(); // Lấy tham số từ URL
@@ -22,6 +23,8 @@ export default function ProductDetail() {
             <div className="detail-name">
                 <h1>Liverpool Kit 2024</h1>
             </div>
+            <RatingStars rating={4}></RatingStars>
+            <h2 className="detail-price">500,000 VND</h2>
         </div>
       </div>
     </div>
